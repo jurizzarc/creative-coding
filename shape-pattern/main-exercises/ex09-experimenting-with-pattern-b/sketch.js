d28d64c659f9403c57fb46d5e65578c27785c253
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-14 00:52:26 +0000

    Update sketch.js of exercise 09

--- shape-pattern/main-exercises/ex09-experimenting-with-pattern-b/sketch.js
+++ shape-pattern/main-exercises/ex09-experimenting-with-pattern-b/sketch.js
@@ -1,17 +1,17 @@
-let squares = [];
+let shapes = [];
 let canvasSize = 500;
-let numOfSquares = 20;
-let squareSize = canvasSize/numOfSquares;
-let start, end, val, col, randomNum;
+let numOfShapes = 20;
+let shapeSize = canvasSize/numOfShapes;
+let col, randomNum;
 
 function setup() {
   createCanvas(canvasSize, canvasSize);
-  colorMode(RGB, numOfSquares, numOfSquares, numOfSquares);
+  colorMode(RGB, numOfShapes, numOfShapes, numOfShapes);
   //noLoop();
 
-  for (let i = 0; i < numOfSquares; i++) {
-    for (let j = 0; j < numOfSquares; j++) {
-      squares.push(new Square(i, j));
+  for (let i = 0; i < numOfShapes; i++) {
+    for (let j = 0; j < numOfShapes; j++) {
+      shapes.push(new Shape(i, j));
     }
   }
 }
@@ -19,37 +19,37 @@ function setup() {
 function draw() {
   background(0);
   randomSeed(23);
-  squares.forEach(square => {
-    square.render();
+  shapes.forEach(shape => {
+    shape.render();
   });
 }
 
-class Square {
+class Shape {
   constructor(_i, _j) {
     this.i = _i;
     this.j = _j;
-    this.s = squareSize;
+    this.s = shapeSize;
     this.x = this.i * this.s; //transX
     this.y = this.j * this.s; //transY
-    this.sw = 1;
   }
 
   render() {
     push();
     translate(this.x, this.y);
     
-    col = color(this.i, 0, numOfSquares - this.j);
-    strokeWeight(this.sw);
-    stroke(col);
+    col = color(this.i, 0, numOfShapes - this.j);
     randomNum = round(random(0, 1));
     if (randomNum == 0) {
-      fill(this.i, 0, numOfSquares - this.j);
+      noStroke();
+      fill(col);
       beginShape(TRIANGLES);
       vertex(0, 0);
       vertex(0, this.s);
       vertex(this.s, 0);
       endShape();
     } else {
+      strokeWeight(1);
+      stroke(col);
       line(0, 0, this.s, this.s);
     }
     
